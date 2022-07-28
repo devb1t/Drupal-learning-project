6ae12ffcbd3bf67616121bee1086da2bd3b8f4df
devb1t <v.galetskyi@dev-branch.com>
2022-07-28 12:36:41 +0300

    #5162 Add image, font loader in webpack

--- web/themes/custom/rebartik/webpack.config.js
+++ web/themes/custom/rebartik/webpack.config.js
@@ -1,19 +1,28 @@
+/* eslint-disable */
+
 const path = require("path");
 const MiniCss = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    javascript: ["./src/js_index.js"]
+    dscss: ["./src/index.js"],
+    javascript: ["./src/js_index.js"],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js"
+    filename: "[name].js",
+    publicPath: '../dist/',
+    clean: true
   },
   module: {
     rules: [
       {
-        test: /\.(s*)css$/,
-        use: [MiniCss.loader, "css-loader", "sass-loader"]
+        test:/\.(s*)css$/,
+        use: [MiniCss.loader, 'css-loader', 'sass-loader',]
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)/,
+        type: 'asset/resource'
       }
     ]
   },
